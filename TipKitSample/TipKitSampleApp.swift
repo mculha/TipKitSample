5350167b21061a3b4728b24d00cb7219cf8d930e
mculha <melih.culha.09@gmail.com>
2023-09-07 19:15:03 +0300

    Initial Commit

---
+++ TipKitSample/TipKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitSampleApp.swift
+//  TipKitSample
+//
+//  Created by Melih Çulha on 7.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
