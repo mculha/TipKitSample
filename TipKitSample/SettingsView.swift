5a42bba845b612accfe103eb92590408a37b7489
mculha <melih.culha.09@gmail.com>
2023-10-02 22:59:28 +0300

    Designs a Sample UI Called SettingView to Show How to Works TipKit

---
+++ TipKitSample/SettingsView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  TipKitSample
+//
+//  Created by Melih Çulha on 7.09.2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @AppStorage("isNotificationsEnabled")
+    private var isNotificationEnabled = true
+    
+    @AppStorage("isSleepTrackingEnabled")
+    private var isSleepTrackingEnabled = true
+    
+    @AppStorage("sleepTrackingMode")
+    private var sleepTrackingMode = SleepTrackingMode.moderate.rawValue
+    
+    @AppStorage("sleepGoal")
+    private var sleepGoal = 8
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Notifications settings")) {
+                    Toggle(isOn: $isNotificationEnabled) {
+                        Text("Notification:")
+                    }
+                }
+
+                Section(header: Text("Sleep tracking settings")) {
+                    Toggle(isOn: $isSleepTrackingEnabled) {
+                        Text("Sleep tracking:")
+                    }
+
+                    Picker(
+                        selection: $sleepTrackingMode,
+                        label: Text("Sleep tracking mode")
+                    ) {
+                        ForEach(SleepTrackingMode.allCases, id: \.self) {
+                            Text($0.rawValue).tag($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+
+                    Stepper(value: $sleepGoal, in: 6...12) {
+                        Text("Sleep goal is \(sleepGoal) hours")
+                    }
+                }
+                
+                Section {
+                    Button("Unlock PRO") {}
+                    Button("Restore purchase") {}
+                }
+            }
+            .navigationBarTitle(Text("Settings"))
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+enum SleepTrackingMode: String, CaseIterable {
+    case low = "low"
+    case moderate = "moderate"
+    case aggressive = "aggressive"
+}
--- TipKitSample/TipKitSampleApp.swift
+++ TipKitSample/TipKitSampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TipKitSampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SettingsView()
         }
     }
 }
